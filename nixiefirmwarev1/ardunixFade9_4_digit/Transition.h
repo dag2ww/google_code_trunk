219013eabc047039756be3b0805ab8ac321b20be
Grzegorz Dancewicz <g.dancewicz@ocado.com>
2020-06-25 13:38:08 +0200

    add final and working version of nixie

---
+++ nixiefirmwarev1/ardunixFade9_4_digit/Transition.h
@@ -0,0 +1,43 @@
+#ifndef Transition_h
+#define Transition_h
+
+#include "Arduino.h"
+#include "DisplayDefs.h"
+
+extern byte NumberArray[];
+extern byte displayType[];
+extern boolean scrollback;
+
+class Transition
+{
+  public:
+    Transition(int, int, int);
+    void start(unsigned long);
+    boolean isMessageOnDisplay(unsigned long);
+    boolean scrollMsg(unsigned long);
+    boolean scrambleMsg(unsigned long);
+    boolean scrollInScrambleOut(unsigned long);
+    void setRegularValues();
+    void setAlternateValues();
+    void loadRegularValues();
+    void loadAlternateValues();
+    void saveCurrentDisplayType();
+    void restoreCurrentDisplayType();
+  private:
+    int _effectInDuration;
+    int _effectOutDuration;
+    int _holdDuration;
+    unsigned long _started;
+    unsigned long _end;
+    byte _regularDisplay[DIGIT_COUNT] = {0, 0, 0, 0};
+    byte _alternateDisplay[DIGIT_COUNT] = {0, 0, 0, 0};
+    boolean _savedScrollback;
+    byte _savedDisplayType[DIGIT_COUNT] = {FADE, FADE, FADE, FADE};
+  
+    unsigned long getEnd();
+    int scroll(int8_t);
+    int scramble(int, byte, byte);
+    unsigned long hash(unsigned long);
+};
+
+#endif
