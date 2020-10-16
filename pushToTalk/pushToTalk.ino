178a291c78fefcd21251ddc133454cce7ac68cf7
Grzegorz Dancewicz <g.dancewicz@ocado.com>
2020-10-16 19:52:15 +0200

    add 8bit games sim

--- pushToTalk/pushToTalk.ino
+++ pushToTalk/pushToTalk.ino
@@ -23,18 +23,31 @@
 #include "Keyboard.h"
 
 const int buttonPin = 3;          // input pin for pushbutton
+const int buttonPin2 = 6;          // input pin for pushbutton
+const int buttonPin3 = 5;   
+const int powerPin = 14;
+
 int previousButtonState = HIGH;   // for checking the state of a pushButton
 
+int previousButtonState2 = HIGH;   // for checking the state of a pushButton
+
+int previousButtonState3 = HIGH;   // for checking the state of a pushButton
 
 void setup() {
   // make the pushButton pin an input:
   pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
+    pinMode(buttonPin3, INPUT_PULLUP);
+    pinMode(powerPin, OUTPUT);
+    digitalWrite(powerPin, HIGH);
   // initialize control over the keyboard:
   Keyboard.begin();
 }
 
 void loop() {
   int buttonState = digitalRead(buttonPin);
+  int buttonState2 = digitalRead(buttonPin2);
+  int buttonState3 = digitalRead(buttonPin3);
 
   if (buttonState != previousButtonState){
     if (buttonState == LOW) {
@@ -44,6 +57,29 @@ void loop() {
       Keyboard.release(KEY_LEFT_CTRL);
       Keyboard.release('1');
     }
-  previousButtonState = buttonState;
+    previousButtonState = buttonState;
+  }
+    if (buttonState2 != previousButtonState2){
+    if (buttonState2 == LOW) {
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press('1');
+    } else {
+      Keyboard.release(KEY_LEFT_CTRL);
+      Keyboard.release('1');
+    }
+    previousButtonState2 = buttonState2;
+  }
+    
+    if (buttonState3 != previousButtonState3){
+    if (buttonState3 == LOW) {
+      Keyboard.press(KEY_LEFT_CTRL);
+      Keyboard.press(KEY_LEFT_SHIFT);
+      Keyboard.press('1');
+    } else {
+      Keyboard.release(KEY_LEFT_CTRL);
+      Keyboard.release(KEY_LEFT_SHIFT);
+      Keyboard.release('1');
+    }
+  previousButtonState3 = buttonState3;
 }
 }
